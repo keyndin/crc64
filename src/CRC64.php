7362aa04a3c790b1c31642853a9a9d6ff02c5abf
Florian Lang <f.lang@mailbox.org>
2022-12-04 17:39:18 +0100

    Use Lookup table

--- src/CRC64.php
+++ src/CRC64.php
@@ -10,8 +10,6 @@ use JetBrains\PhpStorm\Pure;
  */
 class CRC64
 {
-    /** @var int[] */
-    private array $bytes;
     /** @var Format */
     private Format $format = Format::HEX;
     /** @var Polynomial */
@@ -20,12 +18,17 @@ class CRC64
     private ?array $table = null;
     /** @var ?GMP */
     private ?GMP $value = null;
+    /** @var ?int[] */
+    private ?array $bytes = null;
+    private bool $invertIn = true;
 
     /**
-     * @param int[] $bytes
+     * @param GMP $value
+     * @param array $bytes
      */
-    private function __construct(array $bytes)
+    private function __construct(GMP $value, array $bytes = [])
     {
+        $this->value = $value;
         $this->bytes = $bytes;
     }
 
@@ -56,40 +59,68 @@ class CRC64
      */
     private function generateTable(): void
     {
+        $this->table = [];
         $poly = $this->polynomial->toInt();
 
-        for ($i = 0; $i < 256; $i++)
-        {
-            for ($part = gmp_init($i), $bit = 0; $bit < 8; $bit++) {
-                if (($part & 1) == "1") {
-                    $part = gmp_xor((($part >> 1) & ~(0x8 << 60)), $poly);
-                } else {
-                    $part = ($part >> 1) & ~(0x8 << 60);
-                }
+        for ($n = 0; $n < 256; $n++) {
+            $crc = gmp_init($n);
+            for ($k = 0; $k < 8; $k++) {
+                if (($crc & 1) == "1") $crc = (($crc >> 1) ^ $poly) ^ -1966336036761794655;
+                else $crc = ($crc >> 1);
             }
+            $this->table[0][$n] = $crc;
+        }
 
-            $this->table[$i] = $part;
+        for ($n = 0; $n < 255; $n++) {
+            $crc = $this->table[0][$n];
+            for ($k = 0; $k < 8; $k++) {
+                $off = gmp_intval($crc & 0xff);
+                $crc = $this->table[0][$off] ^ ($crc >> 8);
+                $this->table[$k][$n] = $crc;
+            }
         }
     }
 
     public function convert(): self
     {
         if ($this->table === null) $this->generateTable();
+        if ($this->invertIn) $this->value = ~$this->value;
 
-        $this->value = gmp_init(0);
-
-        for ($i = 1; $i <= sizeof($this->bytes); $i++) {
-            $off = gmp_intval(($this->value ^ $this->bytes[$i]) & 0xff);
-            $this->value = gmp_init($this->table[$off] ^ (($this->value >> 8) & ~(0xff << 56)));
+        $idx = 1;
+        $len = sizeof($this->bytes);
+        while ($len >= 8) {
+            $this->value = $this->table[7][gmp_intval($this->value & 0xff ^ ($this->bytes[$idx] & 0xff))]
+                ^ $this->table[6][gmp_intval(($this->value >> 8) & 0xff ^ ($this->bytes[$idx + 1] & 0xff))]
+                ^ $this->table[5][gmp_intval(($this->value >> 16) & 0xff ^ ($this->bytes[$idx + 2] & 0xff))]
+                ^ $this->table[4][gmp_intval(($this->value >> 24) & 0xff ^ ($this->bytes[$idx + 3] & 0xff))]
+                ^ $this->table[3][gmp_intval(($this->value >> 32) & 0xff ^ ($this->bytes[$idx + 4] & 0xff))]
+                ^ $this->table[2][gmp_intval(($this->value >> 40) & 0xff ^ ($this->bytes[$idx + 5] & 0xff))]
+                ^ $this->table[1][gmp_intval(($this->value >> 48) & 0xff ^ ($this->bytes[$idx + 6] & 0xff))]
+                ^ $this->table[0][gmp_intval(($this->value >> 56) & 0xff ^ ($this->bytes[$idx + 7] & 0xff))];
+            $idx += 8;
+            $len -= 8;
         }
 
-
+        while ($len > 0) {
+            $off = gmp_intval($this->value ^ $this->bytes[$idx]) & 0xff;
+            $this->value = $this->table[0][gmp_intval($this->value ^ $this->bytes[$idx]) & 0xff] ^ ($this->value >> 8);
+            $idx++;
+            $len--;
+        }
         return $this;
     }
 
     #[Pure] public static function fromString(string $value): self
     {
-        return new static(unpack('C*', $value));
+        /** @var int[] $bytes */
+        $bytes = unpack('C*', $value);
+        /** @var GMP $val */
+        $val = gmp_init(0);
+        for ($i = 1; $i <= 4; $i++) {
+            $val <<= 8;
+            $val ^= gmp_and($bytes[$i], 0xFF);
+        }
+        return new static($val, $bytes);
     }
 
     public function __toString(): string
--- src/Polynomial.php
+++ src/Polynomial.php
@@ -5,7 +5,7 @@ namespace Keyndin\Crc64;
 enum Polynomial: string
 {
     case ECMA = "0xC96C5795D7870F42";
-    case ISO = "0xD800000000000000";
+    case ISO = "-3932672073523589310";
 
     public function toInt(): \GMP
     {
